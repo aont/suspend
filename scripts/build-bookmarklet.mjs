2dd63cc1a99c6b5863866f939ae91a5d4d73a680
aont <220471+aont@users.noreply.github.com>
2026-05-13 00:15:02 +0900

    Fix bookmarklet href escaping for backslashes
    Added escaping for backslashes in bookmarklet href.

--- scripts/build-bookmarklet.mjs
+++ scripts/build-bookmarklet.mjs
@@ -13,7 +13,7 @@ if (result.error || !result.code) {
   throw result.error || new Error("Terser minification produced no output");
 }
 
-const bookmarkletHref = "javascript:" + result.code.replaceAll("%", "%25").replaceAll("\"", "%22");;
+const bookmarkletHref = "javascript:" + result.code.replaceAll("%", "%25").replaceAll("\"", "%22").replaceAll("\\", "%5C");
 
 const template = fs.readFileSync("public/index.html", "utf8");
 const output = template.replace("__BOOKMARKLET_HREF__", JSON.stringify(bookmarkletHref));
